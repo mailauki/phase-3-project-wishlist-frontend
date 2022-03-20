9f5e21cc653f05d6c344105d4cab0bef40cd8c21
Julie Evans <juliemevans17@gmail.com>
2022-03-19 19:02:18 -0500

    Add Items

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
-
+import React from 'react';
+import { useEffect, useState } from 'react';
 // import './App.css';
-import Header from './Header'
+import Header from './Header';
+import Item from './Item';
 
 function App() {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:9292/items")
+    .then(res => res.json())
+    .then(data => setItems(data))
+  }, [])
+
   return (
     <div className="App">
       <Header />
       <div className="Content">
+        {items.map(item => <Item value={item} />)}
       </div>
     </div>
   );
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,8 +1,13 @@
-function Item() {
+function Item(value) {
+  const {name, price, priority, category, balance} = value.value
   return (
     <div className="Item">
-      <h3></h3>
+      <h3>{name}</h3>
       <div className="ItemContent">
+        <p>Price: {price}</p>
+        <p>Priority: {priority}</p>
+        <p>Category: {category}</p>
+        <p>Balance: {balance.name}</p>
       </div>
     </div>
   )
